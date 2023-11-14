9392bd7ddbf24db31ff29f93adcf6de12c1e668e
lee-JunR <wns2610@icloud.com>
2023-11-15 00:48:09 +0900

    feat : "평일 할인 기능 구현"

---
+++ src/main/java/christmas/domain/WeekdayDiscountStrategy.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+import christmas.constant.Menu;
+import christmas.constant.Menu.FoodCategory;
+import java.util.Map;
+
+public class WeekdayDiscountStrategy implements DiscountStrategy {
+
+  private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
+
+  /**
+   * 평일 할인(일요일~목요일): 디저트 메뉴를 메뉴 1개당 2,023원 할인합니다.
+   *
+   * @param foodItems 메뉴 항목과 수량을 담은 맵.
+   * @return 적용된 총 할인 금액.
+   */
+  @Override
+  public int applyDiscount(Map<Menu, Integer> foodItems) {
+    int mainMenuCount = 0;
+    // 디저트 메뉴의 개수 산출
+    for (Menu key : foodItems.keySet()) {
+      if (key.getCategory() == FoodCategory.DESSERT) {
+        mainMenuCount += foodItems.get(key);
+      }
+    }
+    return mainMenuCount * WEEKDAY_DISCOUNT_AMOUNT;
+  }
+
+}
