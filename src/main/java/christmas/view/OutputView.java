070ec7170c1fa85f3f475f7f99db66d76ce37491
lee-JunR <wns2610@icloud.com>
2023-11-15 02:03:21 +0900

    feat : "이벤트 플래너 시작 문구 출력"

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,11 @@
 package christmas.view;
 
+
 public class OutputView {
 
-  public void printMenu() {
-    System.out.println("<주문 메뉴>");
-    // ...
+
+  public void printEvent(){
+    System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
   }
-  // ...
+
 }
