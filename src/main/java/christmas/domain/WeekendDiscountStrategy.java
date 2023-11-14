ff0b69bb403fbb67609ab0a2de8f1830578ec104
lee-JunR <wns2610@icloud.com>
2023-11-15 00:30:17 +0900

    chore : "다른 할인 전략과 구분 가능하도록 주석 변경"

--- src/main/java/christmas/domain/WeekendDiscountStrategy.java
+++ src/main/java/christmas/domain/WeekendDiscountStrategy.java
@@ -9,7 +9,7 @@ public class WeekendDiscountStrategy implements DiscountStrategy {
   private static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
 
   /**
-   * 메뉴 항목의 주문 수량에 따라 할인을 적용합니다.
+   * 주말 할인(금요일, 토요일): 메인 메뉴를 메뉴 1개당 2,023원 할인합니다.
    *
    * @param foodItems 메뉴 항목과 수량을 담은 맵.
    * @return 적용된 총 할인 금액.
