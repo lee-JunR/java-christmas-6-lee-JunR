f9e56e9f8e4ff4d84babc80e71346f8bdb23f5ba
lee-JunR <wns2610@icloud.com>
2023-11-14 16:49:01 +0900

    chore : "테스트 코드에 DisplayName 추가"

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 import christmas.constant.Menu;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderTest {
 
   @Test
+  @DisplayName("생성된 주문의 예약 일자와 음식 주문 맵 확인")
   void testOrderCreation() {
     // 예약 일자와 음식 주문 맵 생성
     Integer reservationDate = 10;
@@ -21,18 +23,18 @@ class OrderTest {
     // 주문 생성
     Order order = new Order(reservationDate, foodItems);
 
-    // 생성된 주문의 예약 일자와 음식 주문 맵 확인
     assertEquals(reservationDate, order.getReservationDate());
     assertEquals(foodItems, order.getFoodItems());
   }
 
   @Test
+  @DisplayName("잘못된 예약 일자로 주문 생성 시 예외 발생 확인")
   void testInvalidReservationDate() {
-    // 잘못된 예약 일자로 주문 생성 시 예외 발생 확인
     Integer invalidReservationDate = 40;
     Map<Menu, Integer> foodItems = new HashMap<>();
     foodItems.put(Menu.T_BONE_STEAK, 2);
 
-    assertThrows(IllegalArgumentException.class, () -> new Order(invalidReservationDate, foodItems));
+    assertThrows(IllegalArgumentException.class,
+        () -> new Order(invalidReservationDate, foodItems));
   }
 }
