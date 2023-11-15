b9450b959a5efe50ef6d1ad76f5b577be018140c
lee-JunR <wns2610@icloud.com>
2023-11-15 17:48:01 +0900

    refactor : "SPECIALDAY 와 WEEKDAY 둘다 적용 가능하도록 타입 변경"

--- src/main/java/christmas/constant/Calendar.java
+++ src/main/java/christmas/constant/Calendar.java
@@ -4,11 +4,11 @@ public enum Calendar {
 
   // 2023년 12월의 달력
   DECEMBER_2023(new DayType[]{ DayType.WEEKEND, DayType.WEEKEND,
-      DayType.SPECIAL, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
-      DayType.SPECIAL, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
-      DayType.SPECIAL, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
-      DayType.SPECIAL, DayType.SPECIAL, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
-      DayType.SPECIAL}, // 25일은 특별 할인 날
+      DayType.SPECIAL_WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
+      DayType.SPECIAL_WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
+      DayType.SPECIAL_WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
+      DayType.SPECIAL_WEEKDAY, DayType.SPECIAL_WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKDAY, DayType.WEEKEND, DayType.WEEKEND,
+      DayType.SPECIAL_WEEKDAY}, // 25일은 특별 할인 날
       new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
           24, 25, 26, 27, 28, 29, 30, 31}); // 날짜 배열
 
@@ -31,7 +31,8 @@ public enum Calendar {
   public enum DayType {
     WEEKDAY, // 주중
     WEEKEND, // 주말
-    SPECIAL // 특별 할인 날
+    SPECIAL_WEEKDAY, // 특별 할인 + 주중
+    SPECIAL_WEEKEND // 특별 할인 + 주말
   }
 }
 
